b50f2b1198757a08dec51e12e37f6ae6660ceabd
Pauline2602 <paulinelegroux@laposte.net>
2022-05-22 15:38:55 +0200

    add image

--- react/components/app/app.tsx
+++ react/components/app/app.tsx
@@ -3,10 +3,16 @@ import './app.scss'
 import Menu from '../menu/menu'
 
 export default function App() {
+  //<img src="react/../../img/eso.jpg" />
+
   return (
     <>
-      <div className="title">
-        <h1>Eso addons updater</h1>
+      <div className="header">
+        <img
+          className="Eso-img"
+          src="D:\Document\Cours\A4\Eso updater projet\eso-addons-updater\react\img\eso.jpg"
+        />
+        <h1 className="title">Eso addons updater</h1>
       </div>
       <Menu />
     </>
