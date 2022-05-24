95b59b8995e2eaa62d570c486b87934b20374a06
WIEREZ Loïc <loic.wierez@viacesi.fr>
2022-05-24 07:43:47 +0200

    Fixed typo

--- react/components/app/app.tsx
+++ react/components/app/app.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <>
       <div className="header">
         <img className="eso-img" src="img/eso.jpg" />
-        <h1 className="title">Eso addons updater</h1>
+        <h1 className="title">ESO Addons Updater</h1>
       </div>
       <Menu />
     </>
