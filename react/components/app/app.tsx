39a95dfc1dcc2df7e828bbfb7556d97825fff5c5
Pauline2602 <paulinelegroux@laposte.net>
2022-05-29 13:21:22 +0200

    add fix div for menu and header

--- react/components/app/app.tsx
+++ react/components/app/app.tsx
@@ -6,8 +6,10 @@ export default function App() {
   return (
     <>
       <div className="header">
-        <img className="eso-img" src="img/eso.jpg" />
-        <h1 className="title">ESO Addons Updater</h1>
+        <div className="top-bar">
+          <img className="eso-img" src="img/eso.jpg" />
+          <h1 className="title">ESO Addons Updater</h1>
+        </div>
       </div>
       <Menu />
     </>
